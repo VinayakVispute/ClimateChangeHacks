e80c7a2dca15699797bf5bd58a140f4e1f848393
Jayesh Yadav <jayeshyadav528@gmail.com>
2023-04-23 18:11:46 +0530

    Update

--- src/Componets/Cart.jsx
+++ src/Componets/Cart.jsx
@@ -114,7 +114,7 @@ settrees(event.target.value);
       const trackingId=Date.now()+Math.floor(Math.random()*100);
   return (
 <>
-    {Checkout ?(<Order trackingId={trackingId} treeName={treeName}/>):(
+    {Checkout ?(<Order trackingId={trackingId} treeName={name}/>):(
 
 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
 <div className="max-w-3xl mx-auto">
