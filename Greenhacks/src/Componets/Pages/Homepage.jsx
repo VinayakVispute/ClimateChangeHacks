d00f1fc01e36bfdf1a0318b86a8d318c22966f10
Nishitbaria <Nishitbaria@gmail.com>
2023-04-23 12:12:40 +0530

    home section updated

--- Greenhacks/src/Componets/Pages/Homepage.jsx
+++ Greenhacks/src/Componets/Pages/Homepage.jsx
@@ -4,7 +4,7 @@ import MainSec2 from "../MainSec2";
 import MainSec3 from "../MainSec3";   
 import MainSec4 from "../MainSec4";   
 import MainSec5 from "../MainSec5";   
-import CertificateSection from "../CertificateSelection";
+
 
 
 
@@ -14,7 +14,7 @@ export default function Homepage() {
 return (
 <div>
 
-<CertificateSection />
+
 <MainSec/>
 <MainSec2 / >
 <MainSec3 / >
