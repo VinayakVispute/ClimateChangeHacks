15fea78ee68388e032d11dd280d5ba1ec87bb609
Vinayak Vispute <vinayakvispute4@gmail.com>
2023-10-28 23:07:42 -0700

    File Strucutre Added

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./App.css";
+import Navbar from "./Componets/Navbar";
+import EventForm from "./Componets/EventForm";
+import { Route, Routes } from "react-router-dom";
+
+import Homepage from "./Componets/Pages/Homepage";
+import Result from "./Componets/Pages/Result";
+import Card from "./Componets/Card";
+import Description from "./Componets/Description";
+import Cart from "./Componets/Cart";
+import Footer from "./Componets/Footer";
+import Cert from "./Componets/Cert";
+
+export default function App() {
+  return (
+    <div>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/EventForm" element={<EventForm />} />
+        <Route path="/Result" element={<Result />} />
+        <Route path="/Card" element={<Card />} />
+        <Route path="/Description" element={<Description />} />
+        <Route path="/Cart" element={<Cart />} />
+        <Route path="/Certi" element={<Cert />} />
+      </Routes>
+
+      <Footer />
+    </div>
+  );
+}
